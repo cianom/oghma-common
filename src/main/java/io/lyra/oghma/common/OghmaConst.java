0b0cf742873966c7ed15ad21adcb5e02f8ee40b2
cianom <cianom@gmail.com>
2017-01-28 01:10:40 +0000

    - Adding common constants to be shared across libraries.

---
+++ src/main/java/io/lyra/oghma/common/OghmaConst.java
@@ -0,0 +1,21 @@
+package io.lyra.oghma.common;
+
+/**
+ * Constants shared across libraries.
+ *
+ * @author Cian.
+ */
+public class OghmaConst {
+
+    public static final int MIN_RESOLUTION_WIDTH  = 800;
+    public static final int MIN_RESOLUTION_HEIGHT = 600;
+    public static final int MAX_RESOLUTION_WIDTH  = 1600;
+    public static final int MAX_RESOLUTION_HEIGHT = 900;
+
+
+    // Prevent instantiation of the class.
+    private OghmaConst() {
+    }
+
+
+}
